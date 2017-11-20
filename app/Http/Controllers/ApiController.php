3094e0c323df163fd5e4c13bfc75156876bc8a29
杨晓杰 <1127129489@qq.com>
2017-11-20 22:00:42 +0800

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -279,6 +279,7 @@ class ApiController extends Controller
 
     }
 
+
     public function notify(Request $request){
         //var_dump(app_path().'/AliPay/alipay.config.php');exit;
         include app_path().'/AliPay/notify_url.php';
@@ -676,12 +677,13 @@ class ApiController extends Controller
     //充值
     public function recharge(Request $request){
         header('Access-Control-Allow-Origin:*');
+        //生成订单
+
         $openid = $request -> input('openid');
         $price = $request -> input('price');
-        $order_id = $request -> input('order_id');
-
-        //看下多少钱可以买多少点
-        $point = $price;
+        if(!$openid || !$price){
+            return response() -> json(['openid_price'=>'error']);
+        }
 
         if($openid){
             $userinfo = DB::table('user') -> where([
@@ -694,25 +696,50 @@ class ApiController extends Controller
             return response() -> json(['status'=>'error']);
         }
 
+
+        //生成订单编号
+        $out_trade_no = date('Ymd') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
+
+        $sign = file_get_contents('http://m.jhqck.com/al/order/sign?total_fee='.$price.'/out_trade_no/'.$out_trade_no);
+
+
+        if(!$sign){
+            return response() -> json(['sign'=>'error']);
+        }
+
+
+
+
+        //看下多少钱可以买多少点
+        $point = $price;
+
+
+        //记录订单号
+
         DB::table('buylog') -> insert([
             'openid' => $openid,
-            //'prize' => $price,
+            'order_id' => $out_trade_no,
             'point' => $point,
             'created_at' => time()
         ]);
 
+        echo $sign;
         //在user表中加入记录
+        /*
         DB::table('user') -> where([
             'openid' => $openid
         ]) -> update([
             'point' => $userinfo -> point + $point
         ]);
 
+
         //返回最终点数
         $userinfo = DB::table('user') -> where([
             'openid' => $openid
         ]) -> first();
-        return response() -> json(['point'=>$userinfo -> point]);
+        */
+
+        //return response() -> json(['point'=>$userinfo -> point]);
     }
 
 
