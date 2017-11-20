4dc15dc94d8a5d5fd39a57ba5a1bf1b54c810109
杨晓杰 <1127129489@qq.com>
2017-11-21 01:15:07 +0800

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -297,7 +297,7 @@ class ApiController extends Controller
         //var_dump(app_path().'/AliPay/alipay.config.php');exit;
         //include app_path().'/AliPay/notify_url.php';
         //dd($alipay_config);
-
+        file_put_contents(public_path().'/testet.txt',json_encode($_POST));
 
 
 
