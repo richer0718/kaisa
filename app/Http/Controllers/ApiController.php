a1771789fd357f928f6225f79ce2c8bce86de4d5
杨晓杰 <1127129489@qq.com>
2017-11-22 23:14:41 +0800

    debug

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1258,11 +1258,11 @@ class ApiController extends Controller
             return response() -> json(['status'=>'error']);
         }
 
-        $is_set = Cache::has($request -> input('mobile'));
+        /*$is_set = Cache::has($request -> input('mobile'));
         if($is_set){
             //代表重复获取
             return response() -> json(['status'=>'waiting']);
-        }
+        }*/
 
         $api = new \ChuanglanSmsApi();
         $mobile = $request -> input('mobile');
